8fe45c13c576701ac48c4a0d50369aa5bcb56489
hiseoung <hiseoung@hiseoungui-MacBookAir.local>
2021-11-15 22:31:43 +0900

    update

--- LimeAPI.py
+++ LimeAPI.py
@@ -12,12 +12,12 @@ def dataCollect(token, cookie):
 
     # TODO 어떤 위치를 중심으로 받아올지 zoom 설정은 어떻게할지 논의 필요
     params = (
-        ('ne_lat', '37.5'),
-        ('ne_lng', '127.1'),
-        ('sw_lat', '37.4'),
-        ('sw_lng', '127.0'),
-        ('user_latitude', '37.5172'),
-        ('user_longitude', '127.0473'),
+        ('ne_lat', '37.506192'),
+        ('ne_lng', '126.991011'),
+        ('sw_lat', '37.506192'),
+        ('sw_lng', '126.991011'),
+        ('user_latitude', '37.506192'),
+        ('user_longitude', '126.991011'),
         ('zoom', '16'),
     )
 
@@ -38,18 +38,18 @@ def dataCollect(token, cookie):
 
 def breakCollect():
     global df_collected, running
-    df_collected.to_csv("./data/LimeData.csv", index=False)
+    df_collected.to_csv("./LimeData.csv", index=False)
     running = False
     print("설정한 시간이 완료되어 프로그램을 종료합니다.")
     return schedule.CancelJob
 
 
 # 1. OTP코드를 메시지로 받습니다. 전화번호를 URL마지막에 넣어주세요 +8210(핸드폰 번호)
-rsp = requests.get('https://web-production.lime.bike/api/rider/v1/login?phone=%2B821012345678')
+rsp = requests.get('https://web-production.lime.bike/api/rider/v1/login?phone=%2B821086094104')
 
 # 2. 핸드폰으로 입력받은 OTP코드를 login_code에 입력합니다.
 header = {'Content-Type': 'application/json'}
-data = '{"login_code": "680783", "phone": "+821012345678"}'
+data = '{"login_code": "930914", "phone": "+821086094104"}'
 response = requests.post('https://web-production.lime.bike/api/rider/v1/login', headers=header, data=data)
 
 # 3. 입력받은 쿠키와 토큰값을 설정하고 전동킥보드 정보를 요청합니다.
@@ -58,10 +58,10 @@ rsp_cookie = response.cookies.get('_limebike-web_session')
 
 df_collected = pd.DataFrame()
 running = True
-schedule.every(10).minutes.do(dataCollect, rsp_token, rsp_cookie)
+schedule.every(1).minutes.do(dataCollect, rsp_token, rsp_cookie)
 
 # 종료 시간을 입력합니다.
-schedule.every().day.at("00:45").do(breakCollect)
+schedule.every().day.at("21:04").do(breakCollect)
 
 while running:
     schedule.run_pending()
