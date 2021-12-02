851b9e31114e1425402159849e9007b1ed5bc587
kwjinwoo <jung003327@gmail.com>
2021-12-02 20:30:25 +0900

    update pre_data

--- jinwoo/visualizing.py
+++ jinwoo/visualizing.py
@@ -14,6 +14,13 @@ def word_count_bar(df_):
         counts.update(noun.split(','))
 
     most_words = counts.most_common(10)
+    most_sr = most_common_to_sr(most_words)
+
+    sns.barplot(x=most_sr.values, y=most_sr.index, alpha=0.8)
+    plt.gca().set_facecolor('#d9eaff')
+    plt.grid(True, axis='x', alpha=0.5)
+    plt.title('단어 빈도 Top 10')
+    plt.show()
 
 
 def most_common_to_sr(most_common):
